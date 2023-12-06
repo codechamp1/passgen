b856e0f0993ea614fb88633f975b9aee4c0f86f8
codechamp1 <mihaitanase20@gmail.com>
2023-12-06 17:50:32 +0100

    added cors midelware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, Depends, HTTPException
 from app.core.schema import PasswordConfig, PasswordGenerated, StrongnessGenerated
 from app.core.logic import generate_password, password_strongness
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(docs_url=None,
               redoc_url=None)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/passgen/generate")
